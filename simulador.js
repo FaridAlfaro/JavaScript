013374fe8d7c30af265ac06254b04e45423a7653
Farid Alfaro <farix6@gmail.com>
2023-03-01 21:52:04 -0300

    PrimeraSubida Correccion

--- simulador.js
+++ simulador.js
@@ -4,7 +4,7 @@ function AplicarDescuento(precioFinal, descuento){
     const precioConDescuento = precioFinal * descuento;
     return precioConDescuento;
   } else{
-    console.log("El descuento debe ser un numero entre 0.1 y 0.9")
+    alert("El descuento debe ser un numero entre 0.1 y 0.9")
   }
 }
 
@@ -14,15 +14,9 @@ const cuotas = (precioFinal + precioFinal * cft) / nCuotas;
 return cuotas;
 }
 
-function MostrarEnPantalla(array){
-for (let i = 0; i < array.length; i++) {
-  console.log (i + 1 + ". " + array[i]);
-}
-}
-
 function Validar(seleccion, array) {
 while (seleccion < 1 || seleccion > array.length) {
-  seleccion = parseInt(prompt("Opción inválida"));
+  seleccion = parseInt(prompt("Opción inválida, ingrese nuevamente"));
 }
 return seleccion;
 }
@@ -32,34 +26,35 @@ let servicios = [
   { nombre: "Automatización industrial", precio: 15000 },
   { nombre: "Domoticá domiciliaría", precio: 2500 }
 ];
-MostrarEnPantalla(servicios.nombre);
+alert("Servicios disponibles:" + "\n1 -" + servicios[0].nombre + "\n2 -" + servicios[1].nombre + "\n3 -" + servicios[2].nombre);
 
 let servicioSeleccionado = parseInt(prompt("Ingrese el número del servicio que desea comprar:"));
 
-Validar(servicioSeleccionado, servicios)
+servicioSeleccionado = Validar(servicioSeleccionado, servicios)
 
-console.log("Ha seleccionado el servicio " + servicios[servicioSeleccionado - 1].nombre);
+alert("Ha seleccionado el servicio " + servicios[servicioSeleccionado - 1].nombre);
 
 let metodosDePago = ["Crédito", "Débito/TB", "Efectivo"];
 
-MostrarEnPantalla(metodosDePago);
+alert("Por favor seleccione un método de pago:" + "\n1 - Crédito"  + "\n2 - Débito/ transferencia bancaria" + "\n3 - Efectivo (20% off)");
 
 let metodoDePagoSeleccionado = parseInt(prompt("Ingrese el número del método de pago que desea utilizar:"));
 
-Validar(metodoDePagoSeleccionado, metodosDePago);
-console.log("Ha seleccionado el método de pago " + metodosDePago[metodoDePagoSeleccionado - 1]);
+metodoDePagoSeleccionado = Validar(metodoDePagoSeleccionado, metodosDePago);
+
+alert("Ha seleccionado el método de pago " + metodosDePago[metodoDePagoSeleccionado - 1]);
 
 let precioFinal = servicios[servicioSeleccionado - 1].precio;
 
 if (metodoDePagoSeleccionado === 3) {
   AplicarDescuento(precioFinal, 0.8); 
-  console.log("Se ha aplicado un descuento del 20% por pago en efectivo");
-  console.log("El precio final es $" + precioFinal);
+  alert("Se ha aplicado un descuento del 20% por pago en efectivo");
+  alert("El precio final es $" + precioFinal);
 } else if (metodoDePagoSeleccionado == 1) {
     let nCuotas = parseInt(prompt("Ingrese un numero de cuotas 3, 6 o 12"))
     const cuotas = CalculoCuotas (precioFinal, nCuotas);
-    console.log("Se ha aplicado el recargo correspondiente al CFT");
-    console.log("El precio en " + nCuotas +" cuotas es: " + cuotas);
+    alert("Se ha aplicado el recargo correspondiente al CFT");
+    alert("El precio en " + nCuotas +" cuotas es: " + cuotas);
 } else {
-  console.log("El precio final es $" + precioFinal);
+  alert("El precio final es $" + precioFinal);
 }
