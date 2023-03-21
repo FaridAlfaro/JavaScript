da7181493598c72c9d9169ac24dc168530a71070
Farid Alfaro <farix6@gmail.com>
2023-03-20 22:52:51 -0300

    Cambio a selectores

--- simulador_.js
+++ simulador_.js
@@ -1,83 +1,101 @@
-function AplicarDescuento(precioFinal, descuento){
-  while (descuento < 0.1 || descuento > 0.9){
-    descuento = parseFloat(prompt("El descuento debe ser un número entre 0.1 y 0.9"));
+function AplicarDescuento(precioFinal, descuento) {
+  while (descuento < 0.1 || descuento > 0.9) {
+    descuento = parseFloat(
+      prompt("El descuento debe ser un número entre 0.1 y 0.9")
+    );
   }
   let precioConDescuento = precioFinal * descuento;
   return precioConDescuento;
 }
 
 function ValidarCuotas(nCuotas, array) {
-  if (Array.isArray(array)){
+  if (Array.isArray(array)) {
     while (!array.includes(nCuotas)) {
-      nCuotas = parseInt(prompt("Número de cuotas inválido, ingrese nuevamente"));
+      nCuotas = parseInt(
+        prompt("Número de cuotas inválido, ingrese nuevamente")
+      );
     }
   } else {
-    alert("El valor ingresado es invalido")
+    alert("El valor ingresado es invalido");
   }
   return nCuotas;
 }
 
-function CalculoCuotas(precioFinal, nCuotas){
+function CalculoCuotas(precioFinal, nCuotas) {
   const cft = 0.65;
   let cuotas = (precioFinal + precioFinal * cft) / nCuotas;
   return cuotas;
 }
 
-function Validar(seleccion, array) {
-  if (Array.isArray(array)){
-    while (seleccion < 1 || seleccion > array.length) {
-      seleccion = parseInt(prompt("Opción inválida, ingrese nuevamente"));
-    }
-  } else {
-    alert("El valor ingresado es invalido")
-  }
-  return seleccion;
-}
-
-function FiltrarCategorias(servicios, categoria) {
-
-  if (Array.isArray(servicios)) {
-    return servicios.filter(servicio => servicio.categoria === categoria);
-  } else {
-    return [];
-  }
-}
-
-let servicios = [
-  { nombre: "Instalacion domiciliaria básica", precio: 50000, categoria: "Domicilios"},
-  { nombre: "Revisión por fallas o consumo exhesivo", precio: 5000, categoria: "Domicilios"},
-  { nombre: "Estudio de factibilidad para automatizar procesos", precio: 40000, categoria: "Industrias"},
-  { nombre: "Instalacion de portones eléctricos", precio: 20000, categoria: "Domótica"},
-  { nombre: "Iluminación de locales/discotecas", precio: 30000, categoria: "Domótica" },
-  { nombre: "Automatización industrial de proceso conocido", precio: 150000, categoria: "Industrias"},
-  { nombre: "Domoticá domiciliaría", precio: 4500, categoria: "Domótica"}
-];
-
-let categorias = ["Domicilios", "Industrias", "Domótica"];
-let metodosDePago = ["Crédito", "Débito/TB", "Efectivo"];
 let nCuotasPosibles = [3, 6, 12];
+const servicioSelect = document.getElementById("Servicio");
+const metodoPagoSelect = document.getElementById("MetodoPago");
+const cotizarButton = document.getElementById("card__button");
+const categoriaSelect = document.getElementById("Categoria");
 
-let categoriaSeleccionada = parseInt(prompt(`Categorías disponibles:\n${categorias.map((categoria, index) => `${index+1} - ${categoria}`).join('\n')}\nIngrese el número de la categoría que desea ver:`));
-categoriaSeleccionada = Validar(categoriaSeleccionada, categorias);
-let serviciosFiltrados = FiltrarCategorias(servicios, categorias[categoriaSeleccionada - 1]);
+fetch("opciones.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Agregado
+    data.metodosPago.forEach((metodo) => {
+      const option = document.createElement("option");
+      option.text = metodo;
+      metodoPagoSelect.add(option);
+      categoriaSelect.addEventListener("click", () => {
+      servicioSelect.innerHTML = "";
+    data.servicios.forEach((servicio) => {
+      if (categoriaSelect.value === servicio.categoria) {
+        const option = document.createElement("option");
+        option.text = servicio.nombre;
+        servicioSelect.add(option);
+        }});
+      });
+    });
 
-alert(`Servicios disponibles en la categoría ${categorias[categoriaSeleccionada - 1]}:\n${serviciosFiltrados.map(servicio => `${servicio.nombre} - $${servicio.precio}`).join('\n')}`);
+    servicioSelect.addEventListener("change", () => {
+      const servicioSeleccionado = servicioSelect.value;
+      const infoServicioDiv = document.getElementById("infoServicio");
+      let servicioEncontrado = data.servicios.find(
+        (servicio) => servicio.nombre === servicioSeleccionado
+      );
+      let descripcionServicio = servicioEncontrado.descripcion;
+      infoServicioDiv.innerHTML = descripcionServicio;
+    });
+    metodoPagoSelect.addEventListener("change", () => {
+      const metodoPagoSeleccionado = metodoPagoSelect.value;
+      const infoPagoDiv = document.getElementById("infoPago");
+      if (metodoPagoSeleccionado === "Efectivo") {
+        infoPagoDiv.innerHTML =
+          "Se aplica un descuento del 20% por pago en efectivo";
+      } else if (metodoPagoSeleccionado === "Tarjeta de crédito") {
+        infoPagoDiv.innerHTML = "Se aplica el recargo correspondiente al CFT";
+      } else {
+        infoPagoDiv.innerHTML =
+          "El pago por transferencia bancaria utiliza el precio de lista";
+      }
+    });
+    cotizarButton.addEventListener("click", () => {
+      const servicioSeleccionado = servicioSelect.value;
+      const metodoPagoSeleccionado = metodoPagoSelect.value;
+      let servicioEncontrado = data.servicios.find(
+        (servicio) => servicio.nombre === servicioSeleccionado
+      );
+      let precioSeleccionado = servicioEncontrado.precio;
 
-let servicioSeleccionado = parseInt(prompt(`Seleccione un servicio de la categoría ${categorias[categoriaSeleccionada - 1]}:`));
-servicioSeleccionado = Validar(servicioSeleccionado, serviciosFiltrados.map((servicio, index) => index+1));
-servicioSeleccionado = serviciosFiltrados[servicioSeleccionado - 1].nombre;
-let metodoDePagoSeleccionado = parseInt(prompt("Por favor seleccione un método de pago:" + "\n1 - Crédito"  + "\n2 - Débito/ transferencia bancaria" + "\n3 - Efectivo (20% off)" + "\nIngrese el número del método de pago que desea utilizar:"));
-metodoDePagoSeleccionado = Validar(metodoDePagoSeleccionado, metodosDePago);
+      const resultadoDiv = document.getElementById("resultado");
 
-let precioFinal = serviciosFiltrados.find(servicio => servicio.nombre === servicioSeleccionado).precio;
-if (metodoDePagoSeleccionado === 3) {
-  precioFinal = AplicarDescuento(precioFinal, 0.8); 
-  alert("Ha seleccionado el método de pago " + metodosDePago[metodoDePagoSeleccionado - 1] + "\nSe ha aplicado un descuento del 20% por pago en efectivo\nEl precio final es $" + precioFinal);
-} else if (metodoDePagoSeleccionado === 1) {
-    let nCuotas = parseInt(prompt("Ingrese un numero de cuota: 3, 6 o 12"))
-    nCuotas = ValidarCuotas(nCuotas, nCuotasPosibles)
-    let cuotas = CalculoCuotas (precioFinal, nCuotas);
-    alert("Ha seleccionado el método de pago " + metodosDePago[metodoDePagoSeleccionado - 1] + "\nSe ha aplicado el recargo correspondiente al CFT\nEl precio en " + nCuotas +" cuotas es: " + cuotas);
-} else {
-  alert("El precio final es $" + precioFinal);
-}
+      if (metodoPagoSeleccionado === "Efectivo") {
+        precioSeleccionado = AplicarDescuento(precioSeleccionado, 0.8);
+        resultadoDiv.innerHTML =
+          "El precio con descuento es: $" + precioSeleccionado;
+      } else if (metodoPagoSeleccionado === "Tarjeta de crédito") {
+        let nCuotas = parseInt(prompt("Ingrese un numero de cuota: 3, 6 o 12"));
+        nCuotas = ValidarCuotas(nCuotas, nCuotasPosibles);
+        let cuotas = CalculoCuotas(precioSeleccionado, nCuotas);
+        resultadoDiv.innerHTML =
+          "El precio en " + nCuotas + " cuotas es: <br/>$" + cuotas + " por mes";
+      } else {
+        resultadoDiv.innerHTML = "El precio final es $" + precioSeleccionado;
+      }
+    });
+  });
